f27c4b44ceb624012cfdce889605586fddd147e0
Gyula Madarasz <gyula.madarasz@pm.me>
2025-12-09 12:04:32 +0000

    Parameters

---
+++ Parameters.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Initializable.hpp"
+
+#include <string>
+
+using namespace std;
+
+class Parameters: public Initializable {
+public:
+    Parameters(
+        const string& inifname, 
+        bool createIfNotExists = false, 
+        bool throwsIfNotExists = true
+    ):
+        Initializable() 
+    {
+        init(inifname, createIfNotExists, throwsIfNotExists);
+    }
+
+    virtual ~Parameters() {}
+
+    void onLoad() {
+        // TODO ...
+    }
+
+    void onReload() {
+        // TODO ...
+    }
+};
