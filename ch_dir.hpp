df03d4237150d4d43f6e7516b8a6a79ecae57bbd
Gyula Madarasz <gyula.madarasz@pm.me>
2026-03-22 19:04:40 +0000

    clangd

--- ch_dir.hpp
+++ ch_dir.hpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-void ch_dir(const string& dir) {
+inline void ch_dir(const string& dir) {
     if (chdir(dir.c_str()) == -1)
         throw ERROR("Failed to change directory: " + strerror(errno));
 }
